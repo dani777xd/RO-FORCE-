aadb92669ca1b3254f85ad1c1bc780269d3c47d4
dani777xd <51877993+dani777xd@users.noreply.github.com>
2019-06-16 13:55:45 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+  "author": {
+    "name": "Raynos",
+    "email": "raynos2@gmail.com"
+  },
+  "bugs": {
+    "url": "https://github.com/Raynos/dom-walk/issues",
+    "email": "raynos2@gmail.com"
+  },
+  "bundleDependencies": false,
+  "contributors": [
+    {
+      "name": "Jake Verbaten"
+    }
+  ],
+  "dependencies": {},
+  "deprecated": false,
+  "description": "iteratively walk a DOM node",
+  "devDependencies": {
+    "browserify-server": "~0.5.6"
+  },
+  "homepage": "https://github.com/Raynos/dom-walk",
+  "keywords": [],
+  "licenses": [
+    {
+      "type": "MIT",
+      "url": "http://github.com/Raynos/dom-walk/raw/master/LICENSE"
+    }
+  ],
+  "main": "index",
+  "name": "dom-walk",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/Raynos/dom-walk.git"
+  },
+  "scripts": {},
+  "version": "0.1.1"
+}
+client.run(str(os.environ.get('BOT_TOKEN')))
